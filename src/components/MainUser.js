5ac674dac39edde8e23f389e9a103ba2a2f62485
AndreySHSH <laptev.andrey@icloud.com>
2021-09-09 17:40:49 +0300

    test

--- src/components/MainUser.js
+++ src/components/MainUser.js
@@ -156,7 +156,7 @@ class MainUsers extends Component {
     code({node, inline, className, children, ...props}) {
       const match = /language-(\w+)/.exec(className || '')
       return !inline && match ? (
-          <SyntaxHighlighter style={tomorrow} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+          <SyntaxHighlighter style={tomorrow} wrapLongLines={true} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
       ) : (
           <code className={className} {...props}>
             {children}
