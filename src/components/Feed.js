46d61a926671b0386f17f0459c606dc2324a1b6b
AndreySHSH <laptev.andrey@icloud.com>
2021-09-09 17:37:24 +0300

    test

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,6 @@
 import React, { Component }  from "react";
 import ReactMarkdown from 'react-markdown'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { tomorrow as style} from "react-syntax-highlighter/dist/esm/styles/prism"
 import like from "../icon/like.png"
 import look from "../icon/look.png";
@@ -270,7 +270,7 @@ class Feed extends Component {
         code({node, inline, className, children, ...props}) {
             const match = /language-(\w+)/.exec(className || '')
             return !inline && match ? (
-                <SyntaxHighlighter style={style} wrapLongLines={true} language={match[1]} showLineNumbers={true} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+                <SyntaxHighlighter style={style} wrapLongLines={true} language={match[1]} showLineNumbers={false} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
             ) : (
                 <code className={className} {...props}>
                     {children}
