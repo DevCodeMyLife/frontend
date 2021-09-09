88bf9ed10b45af8ad77b6f62f9512379abc87108
AndreySHSH <laptev.andrey@icloud.com>
2021-09-09 16:47:50 +0300

    test

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -270,7 +270,7 @@ class Feed extends Component {
         code({node, inline, className, children, ...props}) {
             const match = /language-(\w+)/.exec(className || '')
             return !inline && match ? (
-                <SyntaxHighlighter style={tomorrow} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+                <SyntaxHighlighter style={tomorrow} language={match[1]} showLineNumbers={true} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
             ) : (
                 <code className={className} {...props}>
                     {children}
