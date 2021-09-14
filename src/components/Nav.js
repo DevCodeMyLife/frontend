8fa8e1bc5e6bf9858d04c37196ca4b15b3b31833
AndreySHSH <laptev.andrey@icloud.com>
2021-09-14 14:16:18 +0300

    test

---
+++ src/components/Nav.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import {Link} from "react-navi";
+import notes from "../icon/notes.png";
+import code from "../icon/code.png";
+import user from "../icon/user.png";
+import messages from "../icon/messages.png";
+
+
+class Nav extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            load: this.props.load,
+            auth: this.props.auth,
+            data: this.props.data
+        };
+    }
+
+    render() {
+        return (
+            <div className="wrapper-vertical-nav" >
+                <div className="nav-item">
+                    <Link className="nav-value" href="/feeds">
+                        <div  className="icon-image" >
+                            <img  src={notes} alt="Новости" />
+                        </div>
+                        <div className="nav-value">
+                            Новости
+                        </div>
+                    </Link>
+                </div>
+                <div
+                    className="nav-item"
+
+                >
+                    <Link className="nav-value" href="/freelances">
+                        <div  className="icon-image" >
+                            <img  src={code} alt="Фриланс" />
+                        </div>
+                        <div className="nav-value">
+                            Фриланс
+                        </div>
+                    </Link>
+                </div>
+                {
+                    !this.state.load ?
+                        <div className="loader-flex">
+                            <div className="loader-small" />
+                        </div>
+                        :
+                        this.state.auth ?
+                            <div>
+                                <div
+                                    className="nav-item"
+
+                                >
+                                    <Link className="nav-value" href={`/user?id=${this.state.data[0].id}`}>
+                                        <div  className="icon-image" >
+                                            <img src={user} alt="messages" />
+                                        </div>
+                                        <div className="nav-value">
+                                            Ваша страница
+                                        </div>
+                                    </Link>
+                                </div>
+                                <div
+                                    className="nav-item"
+
+                                >
+                                    {
+                                        this.state.messagesCount ?
+                                            <div className="counter-notification" id="counter_notification" path="/messages" >
+                                                {
+                                                    this.state.messagesCount > 10 ?
+                                                        "10+"
+                                                        :
+                                                        this.state.messagesCount
+                                                }
+                                            </div>
+                                            :
+                                            null
+                                    }
+                                    <Link className="nav-value" href="/messages">
+                                        <div  className="icon-image" >
+                                            <img src={messages} alt="messages" />
+                                        </div>
+                                        <div className="nav-value">
+                                            Мессенджер
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                            :
+                        null
+                }
+            </div>
+        )
+    }
+}
+
+export default Nav;
