1567e2ed151eddfeeda0b5b4085e663b1c28b084
AndreySHSH <laptev.andrey@icloud.com>
2021-09-17 02:08:02 +0300

    fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -31,6 +31,7 @@ class Nav extends Component{
     }
 
     componentDidMount() {
+
         fetch("/api/authentication", {
             method: "POST",
             body: JSON.stringify({
@@ -49,94 +50,94 @@ class Nav extends Component{
 
 
                     this.centrifuge.setToken(res.token)
-                    this.centrifuge.disconnect()
+                    this.centrifuge.disconnect();
                     this.centrifuge.connect();
 
 
-                    if (this.state.channel)
-                        this.state.channel.unsubscribe(`${this.state.data[0].id}`)
-
-                    let this_ = this
-                    this.setState({ channel:
-                            this.centrifuge.subscribe(`${this_.state.data[0].id}`, function(message) {
-                            console.log("[ private channel connect ]")
-
-                            let event = message.data
-
-                            console.log(event)
-
-                            switch (event.type){
-                                case "event":
-                                    this_.setState({notification_count: event.count })
-                                    this_.state.context.resume().then(() => {
-                                        this_.state.audio.play();
-                                        console.log('Playback resumed successfully');
-                                    });
-                                    toast.info('Вашу заметку посмотрели.', {
-                                        position: "top-center",
-                                        autoClose: 5000,
-                                        hideProgressBar: true,
-                                        closeOnClick: true,
-                                        pauseOnHover: true,
-                                        draggable: true,
-                                        progress: undefined,
-                                    });
-                                    break;
-                                case "comment":
-                                    this_.setState({notification_count: event.count })
-                                    this_.state.context.resume().then(() => {
-                                        this_.state.audio.play();
-                                        console.log('Playback resumed successfully');
-                                    });
-                                    toast.info('Вашу заметку прокомментировали.', {
-                                        position: "top-center",
-                                        autoClose: 5000,
-                                        hideProgressBar: true,
-                                        closeOnClick: true,
-                                        pauseOnHover: true,
-                                        draggable: true,
-                                        progress: undefined,
-                                    });
-                                    break;
-                                case "message":
-                                    if (window.location.pathname.match(/messages/) === null) {
-                                        this_.setState({messagesCount: event.count })
-                                        toast.info('Вам пришло новое сообщение.', {
-                                            position: "top-center",
-                                            autoClose: 5000,
-                                            hideProgressBar: true,
-                                            closeOnClick: true,
-                                            pauseOnHover: true,
-                                            draggable: true,
-                                            progress: undefined,
-                                        });
-                                    }
-                                    break;
-                                case "update":
-                                    fetch("/api/authentication", {
-                                        method: "POST",
-                                        body: JSON.stringify({
-                                            "finger": window.localStorage.getItem("finger")
-                                        })
-                                    })
-                                        .then(response => response.json())
-                                        .then(res => {
-                                            if (res.status.code === 0) {
-                                                this_.setState({
-                                                    auth: true,
-                                                    data: res.data,
-                                                    messagesCount: res.count_message,
-                                                    notification_count: res.notification_count
-                                                });
-                                            }
-                                        })
-
-                                    break;
-                                default:
-                                    console.log("[ unidentified event ]")
-                            }
-                        })
-                    })
+                    // if (this.state.channel)
+                    //     this.state.channel.unsubscribe(`${this.state.data[0].id}`)
+                    //
+                    // let this_ = this
+                    // this.setState({ channel:
+                    //         this.centrifuge.subscribe(`${this_.state.data[0].id}`, function(message) {
+                    //         console.log("[ private channel connect ]")
+                    //
+                    //         let event = message.data
+                    //
+                    //         console.log(event)
+                    //
+                    //         switch (event.type){
+                    //             case "event":
+                    //                 this_.setState({notification_count: event.count })
+                    //                 this_.state.context.resume().then(() => {
+                    //                     this_.state.audio.play();
+                    //                     console.log('Playback resumed successfully');
+                    //                 });
+                    //                 toast.info('Вашу заметку посмотрели.', {
+                    //                     position: "top-center",
+                    //                     autoClose: 5000,
+                    //                     hideProgressBar: true,
+                    //                     closeOnClick: true,
+                    //                     pauseOnHover: true,
+                    //                     draggable: true,
+                    //                     progress: undefined,
+                    //                 });
+                    //                 break;
+                    //             case "comment":
+                    //                 this_.setState({notification_count: event.count })
+                    //                 this_.state.context.resume().then(() => {
+                    //                     this_.state.audio.play();
+                    //                     console.log('Playback resumed successfully');
+                    //                 });
+                    //                 toast.info('Вашу заметку прокомментировали.', {
+                    //                     position: "top-center",
+                    //                     autoClose: 5000,
+                    //                     hideProgressBar: true,
+                    //                     closeOnClick: true,
+                    //                     pauseOnHover: true,
+                    //                     draggable: true,
+                    //                     progress: undefined,
+                    //                 });
+                    //                 break;
+                    //             case "message":
+                    //                 if (window.location.pathname.match(/messages/) === null) {
+                    //                     this_.setState({messagesCount: event.count })
+                    //                     toast.info('Вам пришло новое сообщение.', {
+                    //                         position: "top-center",
+                    //                         autoClose: 5000,
+                    //                         hideProgressBar: true,
+                    //                         closeOnClick: true,
+                    //                         pauseOnHover: true,
+                    //                         draggable: true,
+                    //                         progress: undefined,
+                    //                     });
+                    //                 }
+                    //                 break;
+                    //             case "update":
+                    //                 fetch("/api/authentication", {
+                    //                     method: "POST",
+                    //                     body: JSON.stringify({
+                    //                         "finger": window.localStorage.getItem("finger")
+                    //                     })
+                    //                 })
+                    //                     .then(response => response.json())
+                    //                     .then(res => {
+                    //                         if (res.status.code === 0) {
+                    //                             this_.setState({
+                    //                                 auth: true,
+                    //                                 data: res.data,
+                    //                                 messagesCount: res.count_message,
+                    //                                 notification_count: res.notification_count
+                    //                             });
+                    //                         }
+                    //                     })
+                    //
+                    //                 break;
+                    //             default:
+                    //                 console.log("[ unidentified event ]")
+                    //         }
+                    //     })
+                    // })
 
                 }
                 this.setState({
