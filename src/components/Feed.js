fad4f086cbfe3ccea48cab143621b7f8557d664b
AndreySHSH <laptev.andrey@icloud.com>
2021-09-17 19:01:18 +0300

    test

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -17,7 +17,8 @@ class Feed extends Component {
             result: [],
             auth: false,
             messagesCount: 0,
-            data: null
+            data: null,
+            currentDateTime: new Date().getTime()
         };
     }
 
@@ -339,6 +340,12 @@ class Feed extends Component {
                                                     <div className="feed-item-value" >
                                                         <div key="asldk" className="wrapper-data">
                                                             <div key="aksdlkasd"  className="photo-wrapper">
+                                                                {
+                                                                    (this.state.currentDateTime - new Date(data.last_active_at).getTime()) > 3600 ?
+                                                                        null
+                                                                    :
+                                                                        <div className="online_user" />
+                                                                }
                                                                 <div className="online_user" />
                                                                 <img key="asdmmmmasd" src={data?.photo} alt={data?.id} onClick={(e) => {
                                                                     e.preventDefault();
