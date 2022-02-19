67d4faa4468d6623e46d0416157dd12fefdd89eb
AndreySHSH <laptev.andrey@icloud.com>
2022-02-19 14:42:02 +0300

    notification update code

--- src/components/Task.js
+++ src/components/Task.js
@@ -450,7 +450,7 @@ class Task extends Component {
                                                         <div className="comments-title">Комментарии</div>
                                                         <div className="wall-comments">
                                                             {
-                                                                this.state.comments.length ?
+                                                                this.state.comments?.length ?
                                                                     comments.map(com =>
                                                                         <div>
                                                                             <div
