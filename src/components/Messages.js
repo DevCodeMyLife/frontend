88e2d5ac2b40da63b07d18cc7803268c046026b9
AndreySHSH <laptev.andrey@icloud.com>
2022-02-23 17:37:28 +0300

    redisign head

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -754,7 +754,7 @@ class Messages extends Component {
                                                                             marginBottom: 0,
                                                                             background: "var(--hover-message-dialog)"
                                                                         }}
-                                                                        onClick={() => this.openDialog(chat.c_id)}>
+                                                                        onClick={() => this.openDialog(chat.c_id)} id={chat.c_id}>
                                                                         <div className="photo-wrapper-chat">
                                                                             <img src={chat.avatar_url}
                                                                                  alt={chat.avatar_url}/>
