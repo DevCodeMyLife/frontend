b57c424fddbf76be218ecb570b08af102f7a756f
AndreySHSH <laptev.andrey@icloud.com>
2022-02-27 13:23:41 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM nginx:stable-alpine
+FROM node:alpine
 
 MAINTAINER @AndreySHSH <laptev.andrey@icloud.com>
 
-COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
-COPY build /usr/share/nginx/html
+#COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+#COPY build /usr/share/nginx/html
+COPY . ./app
+WORKDIR ./app
 
 EXPOSE 80
 
-CMD ["nginx", "-g", "daemon off;"]
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const resolve = require('path').resolve;
 app.use(serveStatic(resolve('./build')));
 app.get('/', (req, res) => res.sendFile(resolve('./build/index.html')))
 app.get('/feeds', (req, res) => res.sendFile(resolve('./build/feeds.html')))
+app.get('/post', (req, res) => res.sendFile(resolve('./build/feeds.html')))
+
 
 // app.get('/about', (req, res) => res.sendFile(resolve('./build/about.html')));
 // app.get('/services', (req, res) => res.sendFile(resolve('./build/services.html')));
 
-app.listen(3000, () => console.log('Started on PORT 3000'));
+app.listen(80, () => console.log('Started on PORT 80'));
