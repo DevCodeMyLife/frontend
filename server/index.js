7f8ac8a6a092c3048f28e5974f453b2c6a801034
AndreySHSH <laptev.andrey@icloud.com>
2022-03-02 20:42:39 +0300

    fix server_tokens

--- server/index.js
+++ server/index.js
@@ -21,14 +21,15 @@ const description_any_site_og = '<meta property="og:title" content="">'
 
 app.get(['/', '/feeds', '/post', '/user/*', '/settings', '/messages', '/notification', '/people'], (req, res) => {
     const app = ReactDOMServer.renderToNodeStream(<App/>);
-
-
     const indexFile = path.resolve('./build/index.html');
+
     fs.readFile(indexFile, 'utf8', (err, data) => {
         if (err) {
             console.error('Something went wrong:', err);
             return res.status(500).send('Сейчас мы что то делаем');
         }
+
+        console.log(req.path)
 
         switch (req.path) {
             case "/post":
@@ -43,14 +44,15 @@ app.get(['/', '/feeds', '/post', '/user/*', '/settings', '/messages', '/notifica
                     console.log(`statusCode: ${resq.statusCode}`)
 
                     resq.on('data', d => {
-                        preData(
+                        data = preData(
                             data,
-                            res,
                             app,
                             `${JSON.parse(d).data[0].title} | DevCodeMyLife`,
                             `${JSON.parse(d).data[0].tag}, ${JSON.parse(d).data[0].value.split(' ').join(', ')}`,
                             `${JSON.parse(d).data[0].title.substring(0, 30)}`
                         )
+
+                        res.send(data)
                     })
                 })
 
@@ -59,47 +61,49 @@ app.get(['/', '/feeds', '/post', '/user/*', '/settings', '/messages', '/notifica
                 })
                 reqs.end()
 
-                preData(
+                data = preData(
                     data,
-                    res,
                     app,
                     "Лента Новости | DevCodeMyLife",
                     "новости, заметки, код, программирование, golang, python2, python3, python",
                     "Лента новостей"
                 )
+                res.send(data)
                 break
             case "/feeds":
-                preData(
+                data = preData(
                     data,
-                    res,
                     app,
                     "Лента Новости | DevCodeMyLife",
                     "DevCodeMyLIfe, добро, пожаловать",
                     "Лента новостей"
 
                 )
+
+                res.send(data)
                 break
             default:
-                preData(
+                data = preData(
                     data,
-                    res,
                     app,
                     "Добро пожаловать | DevCodeMyLIfe",
                     "DevCodeMyLIfe, добро, пожаловать",
                     "Социальная сеть для программистов",
                 )
+
+                res.send(data)
         }
     });
 });
 
-function preData(data, res, app, title_render, keywords_render, description_render) {
+function preData(data, app, title_render, keywords_render, description_render) {
     data = data.replace(main, `<div id="root">${app}</div>`)
     data = data.replace(title, `<title>${title_render}</title>`)
     data = data.replace(keywords, `<meta name="keywords" content="${keywords_render}"/>`)
     data = data.replace(description, `<meta name="description" content="${description_render}"/>`)
     data = data.replace(description_any_site_og, `<meta property="og:title" content="${description_render}">`)
 
-    return res.send(data);
+    return data
 }
 
 
