bd4c0d6eec5f109ead0b7e67ef58690c21d23b4a
AndreySHSH <laptev.andrey@icloud.com>
2022-03-03 01:08:59 +0300

    fix server_tokens

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get(['/', '/feeds', '/post', '/user/*', '/settings', '/messages', '/notifica
                     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                     console.log('body:', JSON.parse(body).data); // Print the HTML for the Google homepage.
 
-                    if (error !== null){
+                    if (error !== null || response && response.statusCode === 404){
                         data = preData(
                             data,
                             app,
