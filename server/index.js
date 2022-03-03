82d44971969e48bc038bf5e59ab157956a28355d
AndreySHSH <laptev.andrey@icloud.com>
2022-03-03 03:14:49 +0300

    test

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.get(['/', '/feeds', '/post', '/user/*', '/settings', '/messages', '/notifica
                     console.error('error:', error); // Print the error if one occurred
                     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-                    if (error !== null || response && response.statusCode !== 404){
+                    if (response && response.statusCode === 404){
                         data = preData(
                             data,
                             app,
