9256e5ef0b863fee9586fd0773cc99f452d4221a
AndreySHSH <laptev.andrey@icloud.com>
2022-03-06 23:41:12 +0300

    fix

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -491,9 +491,12 @@ class Feed extends Component {
                                                                 <div className="like_wrapper wrapper-flex-end">
                                                                     {
                                                                         data?.tag ?
-                                                                            <div className="tags-type">
-                                                                                #{data?.tag}
-                                                                            </div>
+                                                                            data.tag?.map(tag =>
+                                                                                <div className="tags-type">
+                                                                                    #{tag?.value}
+                                                                                </div>
+                                                                            )
+
                                                                             :
                                                                             null
                                                                     }
