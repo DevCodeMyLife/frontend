0721160e44db018f5eedc881f0bc13da6b2c7fc2
AndreySHSH <laptev.andrey@icloud.com>
2022-03-06 12:16:12 +0300

    fix

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -51,7 +51,7 @@ class Feed extends Component {
     }
 
     randomColor() {
-        return Math.floor(Math.random()*16777215).toString(16);
+        return "#" + Math.floor(Math.random()*16777215).toString(16);
     }
 
     like(uuid) {
