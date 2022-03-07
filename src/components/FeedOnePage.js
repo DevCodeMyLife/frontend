d9e4775ebfcede8114181cdcaf0c537eb789344d
AndreySHSH <laptev.andrey@icloud.com>
2022-03-07 23:08:09 +0300

    test

--- src/components/FeedOnePage.js
+++ src/components/FeedOnePage.js
@@ -269,7 +269,7 @@ class FeedOnePage extends Component {
                                                     "@type": "ListItem",
                                                     "position": 3,
                                                     "name": data?.title || data?.value?.substring(0, 30),
-                                                    "item": `https://devcodemylife.tech/post?uuid=${data?.ID}`
+                                                    "item": `https://devcodemylife.tech/post/${data?.ID}`
                                                 }
                                             ]
                                         }}/>
