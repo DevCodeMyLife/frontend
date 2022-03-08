cfaec2a2e4f5a15e8cc9bd6ce4aaca81e8910fe4
AndreySHSH <laptev.andrey@icloud.com>
2022-03-08 23:46:05 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style/index.css'
 import App from './components/Index'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Router} from "react-router-dom";
 
 
-ReactDOM.render(<BrowserRouter>React.createElement(App, null)</BrowserRouter>, document.getElementById('root'))
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
