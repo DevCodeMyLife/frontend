b97ebb351e11b5a75665a4de7548ff1fda56fa46
AndreySHSH <laptev.andrey@icloud.com>
2022-03-10 05:29:23 +0300

    show nav notification

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,7 +3,6 @@ import React, {Component} from "react";
 class Balance extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             store: this.props.store
         }
