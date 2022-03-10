20ac648d4df995d5054f688caf5622e4ea7d024d
AndreySHSH <laptev.andrey@icloud.com>
2022-03-10 03:59:02 +0300

    show nav notification

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -14,7 +14,28 @@ class Apps extends Component{
 
     render() {
         return (
-            <div/>
+            <div className="wrapper-apps">
+                <div className="apps-title-block">
+                    <h1>Приложения</h1>
+                </div>
+                <div className="apps-search-block">
+                    <input className="input-default"/>
+                </div>
+                <div className="apps-nav-block">
+                    <div className="tags-wrapper" id="tags-wrapper-default">
+                        <div className="button-default-tag tags-item unselectable button-select" id="top"
+                             action="main">
+                            Мои приложения
+                        </div>
+                        <div className="button-default-tag tags-item unselectable" action="all">
+                            Все
+                        </div>
+                    </div>
+                </div>
+                <div className="apps-content-block">
+
+                </div>
+            </div>
         );
     }
 }
