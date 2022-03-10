4df3d47074b76ce25a49c843e1c3d770ff706e62
AndreySHSH <laptev.andrey@icloud.com>
2022-03-10 05:18:33 +0300

    show nav notification

--- src/components/Apps.js
+++ src/components/Apps.js
@@ -3,9 +3,6 @@ import React, {Component} from "react";
 class Apps extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            name: "React"
-        };
 
         this.state = {
             store: this.props.store
