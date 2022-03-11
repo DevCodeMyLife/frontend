dd09c99428c196ef192be953190a8a3fec7a869d
AndreySHSH <laptev.andrey@icloud.com>
2022-03-11 23:22:11 +0300

    test

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -22,12 +22,12 @@ class Balance extends Component {
             <>
                 <div className="content-wall-views">
                     <div className="feed-wrapper">
-                        <div className="main-place-wrapper">
-                            <p>
-                                <b>Coin - </b> это внутренняя валюта, на нее можно приобрести мерч или потратить на
-                                что-то в приложениях а так же вывести на QIWI кошелек.
-                            </p>
-                        </div>
+                        {/*<div className="main-place-wrapper">*/}
+                        {/*    <p>*/}
+                        {/*        <b>Coin - </b> это внутренняя валюта, на нее можно приобрести мерч или потратить на*/}
+                        {/*        что-то в приложениях а так же вывести на QIWI кошелек.*/}
+                        {/*    </p>*/}
+                        {/*</div>*/}
                         {
                             !state.auth.user.isAuth ?
                                 <div className="loader-wrapper feed-wrapper">
@@ -39,7 +39,7 @@ class Balance extends Component {
                                 <div className="wrapper-balance">
                                     <div className="balance-content-block feed-wrapper-item background-white">
                                         <div className="wrapper-balance-count auth-box-title">
-                                            <span className="balance-count">Ваш баланс: {(parseInt(state.auth.user.data.balance)).toLocaleString('ru')} coins.</span>
+                                            <span className="balance-count">Ваш баланс: {(parseInt(state.auth.user.data.balance)).toLocaleString('ru')} tokens.</span>
                                         </div>
                                         <div className="content-pack-coins">
                                             <CoinsCards price={1}/>
--- src/components/CoinsCard.js
+++ src/components/CoinsCard.js
@@ -34,13 +34,12 @@ class CoinsCards extends Component {
     render() {
         return (
             <div className="coins-item">
-                <div className="coins-item-title">{this.state.price} coins</div>
+                <div className="coins-item-title">{this.state.price} tokens</div>
                 <div className="coins-item-descriptions item-about">
                     <p>
-                        Пакет {this.state.price} coins
+                        Пакет {this.state.price} tokens
                     </p>
                 </div>
-
                 <div onClick={()=>{this.buy(this.state.price)}} className="button-general-page" style={{boxSizing: "border-box"}}>Купить за {this.state.price}₽</div>
             </div>
         )
