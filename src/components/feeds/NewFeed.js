63a0a096f97e91c3c2db65b11f17b422c491ea2f
AndreySHSH <laptev.andrey@icloud.com>
2022-03-11 18:01:17 +0300

    test

--- src/components/feeds/NewFeed.js
+++ src/components/feeds/NewFeed.js
@@ -44,36 +44,37 @@ const colourStyles = {
         ...styles,
         color: '#a9a9a9'
     }),
-    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-        const color = chroma(data.color);
-        return {
-            ...styles,
-            backgroundColor: isDisabled
-                ? undefined
-                : isSelected
-                    ? data.color
-                    : isFocused
-                        ? color.alpha(0.1).css()
-                        : undefined,
-            color: isDisabled
-                ? '#ccc'
-                : isSelected
-                    ? chroma.contrast(color, 'white') > 2
-                        ? 'white'
-                        : 'black'
-                    : data.color,
-            cursor: isDisabled ? 'not-allowed' : 'default',
-
-            ':active': {
-                ...styles[':active'],
-                backgroundColor: !isDisabled
-                    ? isSelected
-                        ? data.color
-                        : color.alpha(0.3).css()
-                    : undefined,
-            },
-        };
-    },
+    // option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    //     ...styles,
+    //     // const color = chroma(data.color);
+    //     // return {
+    //     //     ...styles,
+    //     //     backgroundColor: isDisabled
+    //     //         ? undefined
+    //     //         : isSelected
+    //     //             ? data.color
+    //     //             : isFocused
+    //     //                 ? color.alpha(0.1).css()
+    //     //                 : undefined,
+    //     //     color: isDisabled
+    //     //         ? '#ccc'
+    //     //         : isSelected
+    //     //             ? chroma.contrast(color, 'white') > 2
+    //     //                 ? 'white'
+    //     //                 : 'black'
+    //     //             : data.color,
+    //     //     cursor: isDisabled ? 'not-allowed' : 'default',
+    //     //
+    //     //     ':active': {
+    //     //         ...styles[':active'],
+    //     //         backgroundColor: !isDisabled
+    //     //             ? isSelected
+    //     //                 ? data.color
+    //     //                 : color.alpha(0.3).css()
+    //     //             : undefined,
+    //     //     },
+    //     // };
+    // },
     multiValue: (styles, { data }) => {
         const color = chroma(data.color);
         return {
